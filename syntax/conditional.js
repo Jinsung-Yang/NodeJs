5364aebe692fd171e77d0335db7bbd21dee350cb
y881023 <y881023@gmail.com>
2020-04-04 13:53:44 +0900

    test

---
+++ syntax/conditional.js
@@ -0,0 +1,7 @@
+var args=process.argv;
+console.log(args[2]); 
+console.log('A');
+
+if(args[2]=='1'){
+    console.log('c1');
+}
---
+++ syntax/template.js
@@ -0,0 +1,6 @@
+var name='k8805';
+
+
+var letter = `dear' ${name}\n\ns;alkdfjsl;djf;sldjf;lasdjf;alsdjf;lsdkjf;lasdjf;sladj ${name} sdfjl;sdjkf;sdljf;lsdjkf;as'${name}`;
+console.log(letter);
+
